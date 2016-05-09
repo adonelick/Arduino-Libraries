0a97ad04bfebbeb919e2e287a6e9c8c44c5db3b0
Andrew Donelick <adonelick@hmc.edu>
2016-05-08 20:52:56 -0600

    Added attitude control commands

--- BalloonCommands/BalloonCommands.h
+++ BalloonCommands/BalloonCommands.h
@@ -4,11 +4,17 @@
 
 // Defines the commands to be used in the SPARKY balloon system
 
-#define CUTDOWN                 0
-#define CHANGE_TRANSMIT_RATE    1 // Expects a transmission value
-#define TURN_HEATER_ON          2
-#define TURN_HEATER_OFF         3
-#define MANUAL_HEATER_CONTROL   4 // Expects a transmission value
+#define CUTDOWN                     0
+#define CHANGE_TRANSMIT_RATE        1 // Expects a transmission value
+
+// Heater commands
+#define TURN_HEATER_ON              2
+#define TURN_HEATER_OFF             3
+#define MANUAL_HEATER_CONTROL       4 // Expects a transmission value
+
+// Attitude control commands
+#define TOGGLE_ATTITUDE_CONTROL     5 // Expects a transmission value
+#define SET_YAW                     6 // Expects a transmission value
 
 // Relay controls
 #define SWITCH_RELAYS           100
